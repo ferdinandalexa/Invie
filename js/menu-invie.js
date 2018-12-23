648d698bf6f4dd24cf620b5a4e7d1548b93c3283
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2018-12-22 19:58:44 -0600

    Menu Burger terminado

---
+++ js/menu-invie.js
@@ -0,0 +1,50 @@
+const $body = document.body
+// console.log($body);
+const $buttonBurguer = document.getElementById('buttonBurger');
+// console.log($buttonBurguer);
+const $menuBurger = document.querySelector('.menu');
+// console.log($menuBurger);
+
+let movile = window.matchMedia(`screen and (max-width: 480px)`);
+
+movile.addListener(validate);
+
+function hiddenMenu()
+{
+    $menuBurger.classList.remove('is-active');
+}
+function showMenu()
+{
+    $menuBurger.classList.add('is-active');
+}
+function menuInterection()
+{
+    if ($menuBurger.classList.contains('is-active'))
+    {
+        console.log('hidden');
+        hiddenMenu();
+    }
+    else
+    {
+        console.log('show');
+        showMenu();
+    }
+}
+function validate()
+{
+    if(movile.matches)
+    {
+        console.log(movile.matches);
+        $buttonBurguer.addEventListener('click', menuInterection);
+    }
+    else
+    {
+        console.log(movile.matches);
+        $buttonBurguer.removeEventListener('click', menuInterection);
+    }
+}
+
+var hammertime = new Hammer($body);
+
+hammertime.on('swiperight', showMenu);
+hammertime.on('swipeleft', hiddenMenu);
